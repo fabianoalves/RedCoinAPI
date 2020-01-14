364d39d48dff4c8ab463a6ab8e70da44a4b5b8ba
rteles86 <rteles@outlook.com>
2020-01-13 21:03:20 -0300

    Exclusao diretorio redcoin

--- redcoin/entidade/perfil.go
+++
@@ -1,8 +0,0 @@
-package entidade
-
-//Perfil estrutura que representa a tabela Perfil
-type Perfil struct {
-	IDPerfil        int8   `json:"id"`
-	Perfil          string `json:"perfil" validate:"required"`
-	RegistroApagado bool
-}
--- redcoin/entidade/tipoOperacao.go
+++
@@ -1,8 +0,0 @@
-package entidade
-
-//TipoOperacao estrutura que representa a tabela tipoOperacao
-type TipoOperacao struct {
-	IDTipoOperacao  int8   `json:"id"`
-	Operacao        string `json:"operacao" validate:"required"`
-	RegistroApagado bool
-}
